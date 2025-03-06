5e9f1dde6ad622c28879d4feb7ff1289468d2159
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2025-03-06 15:39:02 +0000

    Added more unit tests; 21/21 passing

--- ChineseRomanisation/ChineseRomanisation.Tests/HanyuPinyinReaderTests.cs
+++ ChineseRomanisation/ChineseRomanisation.Tests/HanyuPinyinReaderTests.cs
@@ -10,6 +10,26 @@ namespace ChineseRomanisation.Tests
     {
         [Theory]
         [InlineData("ma", "ma", "ma")]
+        [InlineData("na", "na", "na")]
+        [InlineData("ba", "ba", "pa")]
+        [InlineData("da", "da", "ta")]
+        [InlineData("ga", "ga", "ka")]
+        [InlineData("pa", "pa", "p'a")]
+        [InlineData("ta", "ta", "t'a")]
+        [InlineData("ka", "ka", "k'a")]
+        [InlineData("za", "za", "tsa")]
+        [InlineData("zha", "zha", "cha")]
+        [InlineData("ja", "ja", "cha")]
+        [InlineData("ca", "ca", "ts'a")]
+        [InlineData("cha", "cha", "ch'a")]
+        [InlineData("qa", "qa", "ch'a")]
+        [InlineData("fa", "fa", "fa")]
+        [InlineData("sa", "sa", "sa")]
+        [InlineData("sha", "sha", "sha")]
+        [InlineData("xa", "xa", "hsa")]
+        [InlineData("ha", "ha", "ha")]
+        [InlineData("la", "la", "la")]
+        [InlineData("ra", "ra", "ja")]
         public void ReadWriteTest1(string input, string output1, string output2)
         {
             var syllables = HanyuPinyinReader.ReadHanyuPinyin(input);
