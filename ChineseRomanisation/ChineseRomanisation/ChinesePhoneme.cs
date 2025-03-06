1def731bbf170aa8f1c18fb8e2057f71860ecb5f
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2025-03-06 13:32:43 +0000

    Project Setup

---
+++ ChineseRomanisation/ChineseRomanisation/ChinesePhoneme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChineseRomanisation
+{
+    public class ChinesePhoneme
+    {
+        public string HanyuPinyin { get; set; }
+        public string WadeGiles { get; set; }
+        public string ZhuyinFuhao { get; set; }
+    }
+}
